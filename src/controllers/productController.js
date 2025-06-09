a34ea7adb891f53619be11dc5001153941f71fb2
DuongIT2k3 <doanduong161@gmail.com>
2025-06-09 23:28:43 +0700

    feat/ add delete update products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,26 +1,65 @@
-export const getListProduct = (req, res) => {
-  console.log("danh sach san pham");
-  return res.send("danh sach san pham");
-};
+import Product from "../models/Product.js"
+import handleAsync from "../utils/handleAsync.js"
+import createResponse from "../utils/response.js"
+import createError from "../utils/error.js"
 
-export const getDetailProduct = (req, res) => {
-  console.log("chi tiet san pham");
-  return res.send("chi tiet san pham");
-};
-export const createProduct = async (req, res) => {
-  try {
-    console.log("tao moi san pham");
-    return res.send("tao moi san pham");
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const createProduct = handleAsync( async (req, res, next) => {
+    const existing = await Product.findOne({ title: req.body.title })
+    if(existing) return next(createError(400, "This product already exists!"))
+    const data = await Product.create(req.body)
+    return res.json(createResponse(true, 201, "Create Product successfully!", data))
+})
 
-export const updateProduct = (req, res) => {
-  console.log("cap nhat san pham");
-  return res.send("cap nhat san pham");
-};
-export const deleteProduct = (req, res) => {
-  console.log("xoa san pham");
-  return res.send("xoa san pham");
-};
+export const getListProduct = handleAsync(async (req,res,next) => {
+    const data = await Product.find()
+    return res.json(createResponse(true, 200, "Get list product successfully!", data))
+})
+
+export const getDetailProduct = handleAsync(async (req, res, next) => {
+    const { id } = req.params
+    if(id){
+        const data = await Product.findById(id)
+        return res.json(createResponse(true, 200, "Get detail product successfully!", data))
+    }
+    next(createError(false, 404, "Not found product!"))
+})
+
+export const updateProduct = handleAsync(async (req, res, next) => {
+    const { id } = req.params
+    if(id){
+        const data = await Product.findByIdAndUpdate(id, req.body)
+        return res.json(createResponse(true, 200, "Update product successfully!", data))
+    }
+    next(createError(false, 404, "Product update failed!"))
+})
+
+export const deleteProduct = handleAsync(async (req, res, next) => {
+    const { id } = req.params
+    if(id){
+        await Product.findByIdAndDelete(id)
+        return res.json(createResponse(true, 200, "Delete successfully!"))
+    }
+    next(createError(false, 404, "Product delete failed!"))
+})
+
+export const softDeleteProduct = handleAsync(async(req, res, next) => {
+    const {id} = req.params
+    if(id){
+        await Product.findByIdAndDelete(id, {
+            deletedAt: new Date()
+        })
+        return res.json(createResponse(true, 200, "Hidden product successfully!"))
+    }
+    next(createError(false, 404, "Hidden product failed!"))
+})
+
+export const restoreProduct = handleAsync(async(req,res,next) => {
+    const {id} = req.params
+    if(id){
+        await Product.findByIdAndUpdate(id, {
+            deletedAt: null
+        })
+        return res.json(createResponse(true, 200, "Restore product successfully!"))
+    }
+    next(createError(false, 404, "Restore product failed!"))
+})
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,38 +1,51 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        description: {
-            type: String,
-        },
-        shortDescription:{
-            type: String,
-        },
-        images: {
-            type: Array,
-        },
-        category: {
-            type: String,
-        },
-        quantity:{
-            type: String,
-        },
-        thumbnail: {
-            type: String,
-        },
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
-)
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    brand: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: [String],
+      default: [],
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-export default mongoose.model("Product", productSchema)
+export default mongoose.model("Product", productSchema);
