e00f1984385e5d826bf183f1901c4b3d09654163
DuongIT2k3 <doanduong161@gmail.com>
2025-06-16 15:32:01 +0700

    crud sub-category

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCategory, deleteCategory, getDetailCategory, getListCategory, softDeleteCategory, updateCategory } from "./category.controller.js";
+import { createCategory, deleteCategory, getDetailCategory, getListCategory, restoreCategory, softDeleteCategory, updateCategory } from "./category.controller.js";
 import categorySchema from "./category.schema.js";
 import validBodyRequest from "../../common/middlewares/validBodyRequest.js";
 
@@ -9,8 +9,9 @@ const categoryRoutes = Router()
 categoryRoutes.get("/", getListCategory)
 categoryRoutes.get("/:id", getDetailCategory)
 
-categoryRoutes.delete("/:id", deleteCategory)
+categoryRoutes.delete("/delete/:id", deleteCategory)
 categoryRoutes.delete("/soft-delete/:id", softDeleteCategory)
+categoryRoutes.patch("/restore/:id", restoreCategory)
 
 categoryRoutes.use(validBodyRequest(categorySchema));
 categoryRoutes.patch("/:id", updateCategory)
