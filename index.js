091e805eccf82d96982ad75cd2681006f20f5339
DuongIT2k3 <doanduong161@gmail.com>
2025-06-16 17:13:04 +0700

    fix env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import router from "./src/routes/index.js";
 import connectDB from "./src/common/configs/db.js";
-import { HOST, PORT } from "./src/common/configs/environments.js";
+import {  PORT } from "./src/common/configs/environments.js";
 import errorHandler from "./src/common/middlewares/errorHandle.js";
 import cors from 'cors'
 import setupSwagger from "./src/common/configs/swagger-config.js";
@@ -20,7 +20,7 @@ app.use("/api", router)
 
 app.use(errorHandler)
 
-app.listen(PORT,HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
-  console.log(`Swagger Docs available at ${HOST}:${PORT}/api-docs`);
+app.listen(PORT,() => {
+  console.log(`Server running at http://${PORT}/`);
+  console.log(`Swagger Docs available at ${PORT}/api-docs`);
 });
